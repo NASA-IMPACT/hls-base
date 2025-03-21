88c211f07af80533ed793da518bf16dec03965dd
Chris Holden <ceholden@gmail.com>
2025-03-21 14:23:19 -0400

    Test rebuilt version of Fmask 4_7

--- run_Fmask.sh
+++ run_Fmask.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/local/MATLAB/application/run_Fmask_4_7.sh /usr/local/MATLAB/R2022b /usr/local/MATLAB/application/AuxiData/ 
+/usr/local/MATLAB/application/run_Fmask_4_7.sh /usr/local/MATLAB/v912 /usr/local/MATLAB/application/AuxiData/
 
